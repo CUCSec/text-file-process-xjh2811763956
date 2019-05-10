44baa192db2f08f5b875a429aa1128dc708696e9
xjh2811763956 <1827585657@qq.com>
2019-05-10 17:53:26 +0800

    简化版

--- text-file-process/hh.py
+++ text-file-process/hh.py
@@ -1,7 +1,6 @@
 with open('log_files/201811123009.log', encoding='utf8')as f:
     n = 0
     for line in f:
-        alist = list(map(str, line.split(' ')))
-        if alist[1] == "学号：201811123009,":
+        if list(line.split(' '))[1] == "学号：201811123009,":
             n += 1
     print(n)
