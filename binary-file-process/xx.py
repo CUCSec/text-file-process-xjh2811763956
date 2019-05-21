8b85f6e01b3af2b00bf20d11c31a947fcdf0edd2
xjh2811763956 <1827585657@qq.com>
2019-05-21 09:11:04 +0800

    修正版

--- binary-file-process/xx.py
+++ binary-file-process/xx.py
@@ -1,7 +1,8 @@
 with open('lenna.bmp', 'r+b') as f:
-    a = [10, 1, 8, 1, 1, 1, 2, 3, 10, 10, 9]
-    f.seek(54 + 2)
-    f.write(b'\x00\x00\x00')
+    a = [2, 10, 1, 8, 1, 1, 1, 2, 3, 10, 10, 9]
+    offset = 54
+
     for i in a:
-        f.seek(i, 1)
+        offset += i * 3
+        f.seek(offset)
         f.write(b'\x00\x00\x00')
